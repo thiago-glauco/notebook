df167d81d0950ab8b8a7dc51fd9a771b7ce0f16c
thiago-glauco <thiagoglauco@yahoo.com.br>
2022-04-15 05:33:36 -0300

    classes

---
+++ models/note.js
@@ -0,0 +1,9 @@
+/**
+ * Just a simple note class
+ */
+export default class Note {
+    title;
+    owner;
+    content;
+    timestamp;
+}
---
+++ models/notebook.js
@@ -0,0 +1,16 @@
+import Note from "./note";
+
+/**
+ * Class used to open a notebook
+ */
+
+export default class Notebook {
+    notes = [];
+    constructor( book ) {
+
+    }
+
+    #loadNotes( ) {
+
+    }
+}
